ae069bc9c7b655639093eae6d64e3b3d31c2f382
wsxchen <3063252513@qq.com>
2026-06-02 16:31:52 +0800

    add yolo框

--- src/RobotController/RobotController.py
+++ src/RobotController/RobotController.py
@@ -1,5 +1,6 @@
 
 import cv2
+import numpy as np
 
 from . FpsCounter import FpsCounter
 from . YoloWrapper import YoloWrapper
@@ -47,6 +48,44 @@ class RobotController(object):
             if box.id != None:
                 if box.id.item() == self.GetTargetId():
                     return box
+
+    def GetBoxCenterDistance(self, depth_frame, x1, y1, x2, y2, image_width, image_height):
+        if depth_frame is None:
+            return None, None
+
+        x1 = max(0, min(image_width - 1, x1))
+        y1 = max(0, min(image_height - 1, y1))
+        x2 = max(0, min(image_width - 1, x2))
+        y2 = max(0, min(image_height - 1, y2))
+        if x2 <= x1 or y2 <= y1:
+            return None, None
+
+        box_width = max(1, x2 - x1)
+        box_height = max(1, y2 - y1)
+        center_x = (x1 + x2) // 2
+        center_y = (y1 + y2) // 2
+        half_width = max(3, box_width // 8)
+        half_height = max(3, box_height // 8)
+
+        rx1 = max(0, center_x - half_width)
+        ry1 = max(0, center_y - half_height)
+        rx2 = min(image_width - 1, center_x + half_width)
+        ry2 = min(image_height - 1, center_y + half_height)
+
+        distances = []
+        step_x = max(1, (rx2 - rx1) // 8)
+        step_y = max(1, (ry2 - ry1) // 8)
+
+        for y in range(ry1, ry2 + 1, step_y):
+            for x in range(rx1, rx2 + 1, step_x):
+                distance = depth_frame.get_distance(x, y)
+                if distance > 0:
+                    distances.append(distance)
+
+        if len(distances) == 0:
+            return None, (rx1, ry1, rx2, ry2)
+
+        return float(np.median(distances)), (rx1, ry1, rx2, ry2)
     
     def InputAndProcess(self, frame):
         key = cv2.waitKey(1)
@@ -72,7 +111,7 @@ class RobotController(object):
                 self.SetIsTracking(False)
         frame = cv2.putText(frame, self.id_str, (20, 150), cv2.FONT_HERSHEY_PLAIN, 2, [255, 0, 0], 2)
 
-    def TrackAndDraw(self, frame, box):
+    def TrackAndDraw(self, frame, box, depth_frame=None):
         shape = frame.shape
         frame = cv2.UMat(frame)
 
@@ -87,6 +126,8 @@ class RobotController(object):
             x2 = int(box.xyxy[0][2].item())
             y2 = int(box.xyxy[0][3].item())
             center=((x1+x2)//2, (y1+y2)//2)
+            person_distance, depth_region = self.GetBoxCenterDistance(
+                depth_frame, x1, y1, x2, y2, shape[1], shape[0])
             cv2.circle(frame,center,2,[0,0,255],-1) # 画出选框中心点
 
             #cal radian_velocity
@@ -108,10 +149,17 @@ class RobotController(object):
             label = '{}{:d}'.format("", self.GetTargetId())
             t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 2 , 2)[0]
             cv2.rectangle(frame, (x1, y1), (x2, y2), [255,128,128], 2)
+            if depth_region is not None:
+                rx1, ry1, rx2, ry2 = depth_region
+                cv2.rectangle(frame, (rx1, ry1), (rx2, ry2), [0,255,0], 2)
             cv2.rectangle(frame,(x1, y1),(x1+t_size[0]+3,y1+t_size[1]+4), [255,128,128],-1)
             cv2.putText(frame,label,(x1,y1+t_size[1]+4), cv2.FONT_HERSHEY_PLAIN, 2, [255,255,255], 2)
             cv2.putText(frame,"{:.02f} m/s".format(linear_velocity),(0,250), cv2.FONT_HERSHEY_PLAIN, 2, [255,0,0], 3)
             cv2.putText(frame,"{:.02f} rad/s".format(radian_velocity),(0,300), cv2.FONT_HERSHEY_PLAIN, 2, [255,0,0], 3)
+            if person_distance is not None:
+                cv2.putText(frame,"person depth {:.02f} m".format(person_distance),(0,350), cv2.FONT_HERSHEY_PLAIN, 2, [255,0,0], 3)
+            else:
+                cv2.putText(frame,"person depth invalid",(0,350), cv2.FONT_HERSHEY_PLAIN, 2, [0,0,255], 3)
 
             #pub cmdvel
             if kUseRos1Transfer:
@@ -140,17 +188,12 @@ class RobotController(object):
         cv2.putText(frame,"{:.02f} rad/s".format(0),(0,300), cv2.FONT_HERSHEY_PLAIN, 2, [255,0,0], 3)    
         return frame
 
-    def Run(self, frame):
+    def Run(self, frame, depth_frame=None):
         results = self.yolo_wrapper.Track(frame)
         if(len(results)>0):
             if(self.GetIsTracking()):
                 box = self.FindTarget(results[0].boxes)                
-                return self.TrackAndDraw(frame, box)
+                return self.TrackAndDraw(frame, box, depth_frame)
             else:
                 frame = results[0].plot()
                 return self.NonTrackAndDraw(frame)
-
-
-
-
-
--- src/run_tracker.py
+++ src/run_tracker.py
@@ -28,15 +28,9 @@ while break_flag != True:
     ####################
     # control the robot
     ####################
-    final_frame = robot_controller.Run(bgr_frame)
+    final_frame = robot_controller.Run(bgr_frame, depth_frame)
     if type(final_frame) == type(None):
         continue
-    if depth_frame is not None:
-        center_x = bgr_frame.shape[1] // 2
-        center_y = bgr_frame.shape[0] // 2
-        center_distance = depth_frame.get_distance(center_x, center_y)
-        cv2.putText(final_frame, "center depth {:.02f} m".format(center_distance), (0, 350),
-                    cv2.FONT_HERSHEY_PLAIN, 2, [255, 0, 0], 3)
 
     #################
     # show final img
